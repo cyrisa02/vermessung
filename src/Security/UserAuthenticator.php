c4c78eb595be69874e889b7f8dd3026c743420bb
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-11-30 08:52:59 +0100

    Add Kunden CRUD

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -48,7 +48,8 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('home.index'));
     }
 
     protected function getLoginUrl(Request $request): string
